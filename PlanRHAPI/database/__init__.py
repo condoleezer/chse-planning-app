3ed6cb5c78e47c3d05ce948e153a14ff81a78986
loickkk2001 <loick.eyoum@2027.ucac-icam.com>
2026-01-14 11:55:17 +0100

    Remove MongoDB data from repo

--- PlanRHAPI/database/__init__.py
+++
@@ -1,8 +0,0 @@
-"""PlanRHAPI.database package initializer.
-
-Creating this file ensures the `database` directory is treated as a Python package
-when the app runs inside Docker. This makes imports like `from database.database import db`
-work reliably across different Python import path behaviors.
-"""
-
-from .database import *
